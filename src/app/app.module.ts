1928aa3a455e02f0ec04c6ba556e862c00b414f8
DJ-991 <diwakarjoshi991@gmail.com>
2022-07-19 13:16:14 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+// Reactive-Forms Module
 import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
 // Http-Client Module
 import { HttpClientModule } from '@angular/common/http';
 
-import { StockCardComponent } from './components/stock-card/stock-card.component';
 // Routing Module
 import { AppRoutingModule } from './app-routing.module';
 
+// All Components
+import { AppComponent } from './app.component';
+import { StockCardComponent } from './components/stock-card/stock-card.component';
 import { StockTrackerComponent } from './components/stock-tracker/stock-tracker.component';
 import { StockSentimentComponent } from './components/stock-sentiment/stock-sentiment.component';
 
